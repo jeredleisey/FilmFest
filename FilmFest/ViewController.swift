418beff935dc973026f7a3d854106ab2762dd2e5
Jered D. Leisey <jered.leisey@gmail.com>
2020-10-09 14:58:48 -0600

    Initial Commit

---
+++ FilmFest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmFest
+//
+//  Created by Jered Leisey on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
